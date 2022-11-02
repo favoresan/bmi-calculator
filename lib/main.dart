f03c8ce47be15939592bbf4ac8e6fed85466f92d
favouresan <esanfavour@gmail.com>
2022-11-02 16:02:39 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:bmi_calc_remake/screens/first_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
